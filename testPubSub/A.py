672b9f89573c1f5f3b3afb70411eea0a480fb1fb
RIAPS App Developer <riaps@riapsvbox>
2018-05-21 14:58:49 -0500

    Added starter files for various port types and limits

---
+++ testPubSub/A.py
@@ -0,0 +1,42 @@
+#
+from riaps.run.comp import Component
+import logging
+import os
+import sys
+
+class A(Component):
+    def __init__(self):
+        super(A, self).__init__()
+
+        self.logpath = '/tmp/A.log'
+        self.logFile = open(self.logpath, 'w')
+        self.logFile.write('Actor A started\n')
+
+        self.messageCounter = 0
+
+    def on_clock(self):
+        msg = self.clock.recv_pyobj()
+
+        msg = self.messageCounter
+        self.messageCounter += 1
+
+        self.pubPort.send_pyobj(msg)
+        self.logFile.write("Publish: %s\n" % msg)
+
+        self.clock.halt()
+
+    def on_subPort(self):
+        msg = self.subPort.recv_pyobj()
+
+        self.logFile.write("Subscribe: %s\n" % msg)
+
+        if (self.messageCounter < 10):
+            msg = self.messageCounter
+            self.messageCounter += 1
+
+            self.pubPort.send_pyobj(msg)
+            self.logFile.write("Publish: %s\n" % msg)
+
+    def __destroy__(self):
+        self.logFile.write('Destroying...')
+        self.logFile.close()
