fd84f90004ef04ffe733b8d14c8d395bf09ac889
timkrentz <tskrentz@gmail.com>
2018-05-30 14:13:32 -0500

    testCltSrv: improved logging, added testScript

--- testCltSrv/Server.py
+++ testCltSrv/Server.py
@@ -3,17 +3,30 @@ from riaps.run.comp import Component
 import logging
 import os
 
-class Server(Component):
+class Server(Component, logfile):
     def __init__(self):
         super(Server, self).__init__()
+
+        logpath = '/tmp/' + logfile + '_CompSrv.log'
+        try:
+            os.remove(logpath)
+        except OSError:
+            pass
+
+        self.testlogger = logging.getLogger(__name__)
+        self.testlogger.setLevel(logging.DEBUG)
+        self.fh = logging.FileHandler(logpath)
+        self.fh.setLevel(logging.DEBUG)
+        self.testlogger.addHandler(self.fh)
+
         self.pid = os.getpid()
 
     def on_srvRepPort(self):
         msg = self.srvRepPort.recv_pyobj()
-        self.logger.info("[%d] on_srvRepPort():%s" %(self.pid, msg))
+        self.testlogger.info("[%d] on_srvRepPort():%s" %(self.pid, msg))
         rep = "clt_req: %d" % self.pid
-        self.logger.info("[%d] send rep: %s" % (self.pid,rep)) 
+        self.testlogger.info("[%d] send rep: %s" % (self.pid,rep))
         self.srvRepPort.send_pyobj(rep)
 
     def __destroy__(self):
-        self.logger.info("[%d] destroyed" % self.pid)
+        self.testlogger.info("[%d] destroyed" % self.pid)
