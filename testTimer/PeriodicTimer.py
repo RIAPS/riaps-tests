cbf07e42572c20df68d63f2602bdca634e19a822
timkrentz <tskrentz@gmail.com>
2018-07-10 18:54:30 -0500

    Small fixes
    Fixes include:
    - Fixing inheritance and constructor args
    - Renaming logger for uniformity

--- testTimer/PeriodicTimer.py
+++ testTimer/PeriodicTimer.py
@@ -4,8 +4,8 @@ import logging
 import random
 import os
 
-class PeriodicTimer(Component, logfile):
-    def __init__(self):
+class PeriodicTimer(Component):
+    def __init__(self, logfile):
         super(PeriodicTimer, self).__init__()
 
         logpath = '/tmp/' + logfile + '_CompPeriodic.log'
@@ -14,23 +14,22 @@ class PeriodicTimer(Component, logfile):
         except OSError:
             pass
 
-        self.testlogger = logging.getLogger(__name__)
-        self.testlogger.setLevel(logging.DEBUG)
         self.fh = logging.FileHandler(logpath)
         self.fh.setLevel(logging.DEBUG)
-        self.testlogger.addHandler(self.fh)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
 
     def on_periodic(self):
         now = self.periodic.recv_pyobj()                # Receive time (as float)
-        self.testlogger.info('on_periodic():%s',now)
+        self.logger.info('on_periodic():%s',now)
         period = self.periodic.getPeriod()              # Query the period
         if period == 5.0:
             period = period - 1.0
             self.periodic.setPeriod(period)             # Set the period
-            self.testlogger.info('setting period to %f',period)
+            self.logger.info('setting period to %f',period)
         msg = now
         self.ticker.send_pyobj(msg)
 
 
     def __destroy__(self):
-        self.testlogger.info('Destroying...')
+        self.logger.info('Destroying...')
--- testTimer/SporadicTimer.py
+++ testTimer/SporadicTimer.py
@@ -4,8 +4,8 @@ import logging
 import random
 import os
 
-class SporadicTimer(Component, logfile):
-    def __init__(self):
+class SporadicTimer(Component):
+    def __init__(self, logfile):
         super(SporadicTimer, self).__init__()
 
         logpath = '/tmp/' + logfile + '_CompSporadic.log'
@@ -14,27 +14,26 @@ class SporadicTimer(Component, logfile):
         except OSError:
             pass
 
-        self.testlogger = logging.getLogger(__name__)
-        self.testlogger.setLevel(logging.DEBUG)
         self.fh = logging.FileHandler(logpath)
         self.fh.setLevel(logging.DEBUG)
-        self.testlogger.addHandler(self.fh)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
 
     def on_sporadic(self):
         now = self.sporadic.recv_pyobj()            # Receive time (as float)
-        self.testlogger.info("on_sporadic() %s",now)
+        self.logger.info("on_sporadic() %s",now)
         if self.sporadic.getDelay() == 1.0:         # If delay = 1.0 (i.e. the first firing)
             self.sporadic.setDelay(4.0)             # Change delay to 4.0
             self.sporadic.launch()                  # Launch it again
 
     def on_ticker(self):
         now = self.ticker.recv_pyobj()              # Receive message
-        self.testlogger.info('on_ticker():%s',now)
+        self.logger.info('on_ticker():%s',now)
         if self.sporadic.running():
-            self.testlogger.info("canceling sporadic")
+            self.logger.info("canceling sporadic")
             self.sporadic.cancel()
         self.sporadic.setDelay(1.0)                 # Set the sporadic timer delay
         self.sporadic.launch()                      # Launch the sproadic timer
 
     def __destroy__(self):
-        self.testlogger.info('Destroying...')
+        self.logger.info('Destroying...')
