2efe2cb986187451ae61528e525efadf33837f0d
timkrentz <tskrentz@gmail.com>
2018-07-11 17:09:46 -0500

    Small fixes
    - Fixed botched constructor and inheritance args
    - Unified logging
    - Added test script

--- testCltSrv/Client.py
+++ testCltSrv/Client.py
@@ -3,8 +3,8 @@ from riaps.run.comp import Component
 import os
 import logging
 
-class Client(Component,logfile):
-    def __init__(self):
+class Client(Component):
+    def __init__(self, logfile):
         super(Client, self).__init__()
 
         logpath = '/tmp/' + logfile + '_CompClt.log'
@@ -13,27 +13,26 @@ class Client(Component,logfile):
         except OSError:
             pass
 
-        self.testlogger = logging.getLogger(__name__)
-        self.testlogger.setLevel(logging.DEBUG)
         self.fh = logging.FileHandler(logpath)
         self.fh.setLevel(logging.DEBUG)
-        self.testlogger.addHandler(self.fh)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
 
         self.pid = os.getpid()
         self.pending = 0
 
     def on_clock(self):
         now = self.clock.recv_pyobj()   # Receive time.time() as float
-        self.testlogger.info('on_clock(): %s',str(now))
+        self.logger.info('on_clock(): %s',str(now))
         msg = "clt_req: %d" % self.pid
         if self.pending == 0:
-            self.testlogger.info('[%d] send req: %s' % (self.pid,msg))
+            self.logger.info('[%d] send req: %s' % (self.pid,msg))
             if self.cltReqPort.send_pyobj(msg):
                 self.pending += 1
         else:
             rep = self.cltReqPort.recv_pyobj()
             self.pending -= 1
-            self.testlogger.info('[%d] recv rep: %s' % (self.pid,rep))
+            self.logger.info('[%d] recv rep: %s' % (self.pid,rep))
 
     def __destroy__(self):
-        self.testlogger.info("[%d] destroyed" % self.pid)
+        self.logger.info("[%d] destroyed" % self.pid)
--- testCltSrv/Server.py
+++ testCltSrv/Server.py
@@ -3,8 +3,8 @@ from riaps.run.comp import Component
 import logging
 import os
 
-class Server(Component, logfile):
-    def __init__(self):
+class Server(Component):
+    def __init__(self, logfile):
         super(Server, self).__init__()
 
         logpath = '/tmp/' + logfile + '_CompSrv.log'
@@ -13,20 +13,19 @@ class Server(Component, logfile):
         except OSError:
             pass
 
-        self.testlogger = logging.getLogger(__name__)
-        self.testlogger.setLevel(logging.DEBUG)
         self.fh = logging.FileHandler(logpath)
         self.fh.setLevel(logging.DEBUG)
-        self.testlogger.addHandler(self.fh)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
 
         self.pid = os.getpid()
 
     def on_srvRepPort(self):
         msg = self.srvRepPort.recv_pyobj()
-        self.testlogger.info("[%d] on_srvRepPort():%s" %(self.pid, msg))
+        self.logger.info("[%d] on_srvRepPort():%s" %(self.pid, msg))
         rep = "clt_req: %d" % self.pid
-        self.testlogger.info("[%d] send rep: %s" % (self.pid,rep))
+        self.logger.info("[%d] send rep: %s" % (self.pid,rep))
         self.srvRepPort.send_pyobj(rep)
 
     def __destroy__(self):
-        self.testlogger.info("[%d] destroyed" % self.pid)
+        self.logger.info("[%d] destroyed" % self.pid)
