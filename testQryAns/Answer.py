9ae7a2c06edfd2d469f5b43e664f9760c8c92561
timkrentz <tskrentz@gmail.com>
2018-07-13 11:49:47 -0500

    Added files and fixes to make test run
    Added:
    - test script
    - depl file
    
    Fixed:
    - Component level logging
    - Removed timing requirement

--- testQryAns/Answer.py
+++ testQryAns/Answer.py
@@ -4,19 +4,28 @@ import logging
 import os
 
 class Answer(Component):
-    def __init__(self):
+    def __init__(self, logfile):
         super(Answer, self).__init__()
         self.pid = os.getpid()
+
+        logpath = '/tmp/' + logfile + '_CompAns.log'
+        try:
+            os.remove(logpath)
+        except OSError:
+            pass
+
+        self.fh = logging.FileHandler(logpath)
+        self.fh.setLevel(logging.DEBUG)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
+
+        self.logger.info("(PID %s) - starting CompAns",str(self.pid))
 
     def on_srvAnsPort(self):
         msg = self.srvAnsPort.recv_pyobj()
         self.logger.info("[%d] on_srvAnsPort():%s" %(self.pid, msg))
-        sendTime = self.srvAnsPort.get_sendTime()
-        recvTime = self.srvAnsPort.get_recvTime()
-        self.logger.info("QryReq recv'd @ %f, sent @ %f, diff = %f" 
-                         % (recvTime,sendTime,recvTime-sendTime))
         rep = "clt_qry: %d" % self.pid
-        self.logger.info("[%d] send ans: %s" % (self.pid,rep)) 
+        self.logger.info("[%d] send ans: %s" % (self.pid,rep))
         self.srvAnsPort.send_pyobj(rep)
 
     def __destroy__(self):
--- testQryAns/Query.py
+++ testQryAns/Query.py
@@ -4,25 +4,33 @@ import os
 import logging
 
 class Query(Component):
-    def __init__(self):
+    def __init__(self, logfile):
         super(Query, self).__init__()
         self.pid = os.getpid()
-        
+
+        logpath = '/tmp/' + logfile + '_CompQry.log'
+        try:
+            os.remove(logpath)
+        except OSError:
+            pass
+
+        self.fh = logging.FileHandler(logpath)
+        self.fh.setLevel(logging.DEBUG)
+        self.fh.setFormatter(self.logformatter)
+        self.logger.addHandler(self.fh)
+
+        self.logger.info("(PID %s) - starting CompQry",str(self.pid))
+
     def on_clock(self):
         now = self.clock.recv_pyobj()   # Receive time.time() as float
         self.logger.info('on_clock(): %s',str(now))
         msg = "clt_qry:%d" % self.pid
         self.logger.info('[%d] send qry: %s' % (self.pid,msg))
         self.cltQryPort.send_pyobj(msg)
-    
+
     def on_cltQryPort(self):
         rep = self.cltQryPort.recv_pyobj()
         self.logger.info('[%d] recv rep: %s' % (self.pid,rep))
-        sendTime = self.cltQryPort.get_sendTime()
-        recvTime = self.cltQryPort.get_recvTime()
-        self.logger.info("AnsRep recv'd @ %f, sent @ %f, diff = %f" 
-                         % (recvTime,sendTime,recvTime-sendTime))
 
     def __destroy__(self):
         self.logger.info("[%d] destroyed" % self.pid)
-
