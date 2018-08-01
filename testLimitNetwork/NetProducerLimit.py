c1441dd56ead986035ed9b7cc6349ec98d55b88f
Joey Holliday <joeyholliday77@gmail.com>
2018-08-01 14:04:50 -0500

    Implement Network Limit test

--- testLimitNetwork/NetProducerLimit.py
+++ testLimitNetwork/NetProducerLimit.py
@@ -2,27 +2,48 @@
 from riaps.run.comp import Component
 import logging
 import os
+import random
 
 class NetProducerLimit(Component):
     def __init__(self):
         super(NetProducerLimit, self).__init__()
-        self.pid = os.getpid()
-        self.logger.info("NetProducer [%d]" % self.pid)
-        self.blk = 512
-        self.min = 1*self.blk
-        self.max = 4*self.blk
-        self.size = self.min
+        self.id = random.randint(0,10000)
+        logpath = '/tmp/riaps_NetLimit_%d.log' % self.id
+        try:
+            os.remove(logpath)
+        except OSError:
+            pass
+
+        self.fh = logging.FileHandler(logpath)
+        self.fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(message)s")
+        self.fh.setFormatter(formatter)
+        self.logger.addHandler(self.fh)
+
+        self.logger.info("Starting NetLimit %d" % self.id)
+
+        self.blk = 256
+        self.size = 0
+
+        self.ticks = 0
         
     def on_ticker(self):
+        self.ticks += 1
+        self.size = self.size + self.blk
+
         now = self.ticker.recv_pyobj()   # Receive time.time() as float
-        self.logger.info('on_ticker()[%d]: %s',self.pid,str(now))
+        self.logger.info('Tick %d' % self.size)
         msg = bytearray(self.size)
         self.produce.send_pyobj(msg)
-        self.size = self.size + self.blk
-        if self.size == self.max: self.size = self.min
         
     def handleNetLimit(self):
-        self.logger.info('handleNetLimit')
-        self.size = self.min
+        # Ignore double callbacks
+        if self.ticks>2:
+            self.logger.info('Limit %d' % self.size)
+            self.size = 0
+            self.ticks = 0
+
+    def __destroy__(self):
+        self.logger.info("Stopping NetLimit %d" % self.id)
         
         
