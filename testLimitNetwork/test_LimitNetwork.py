80cb439decb548d4543185f46d121ce859792024
Joey Holliday <joeyholliday77@gmail.com>
2018-08-03 16:48:11 -0500

    Fix LimitNetwork test

--- testLimitNetwork/test_LimitNetwork.py
+++ testLimitNetwork/test_LimitNetwork.py
@@ -19,14 +19,14 @@ def test_LimitNetwork():
                     numTick += 1
                 if line.find("Limit") != -1 and numTick != 0: # Check if a tick has occurred to ignore startup process
                     if numLimit == 0: # Reset numTick on first hit of limit
-                        numTick = 5
+                        numTick = 6
                     numLimit += 1
 
         assert numLimit != 0, "Failed to ever hit limit!"
         
-        # The ratio should be approximately 5.0 because the limit is 1.2KB/s and
+        # The ratio should be approximately 6.0 because the limit is 1.2KB/s and
         # the message size is increased by 256MB every second
         ratio = numTick*1.0/numLimit
         print("numTick=%d numLimit=%d ratio=%.2f" % (numTick, numLimit, ratio))
-        assert ratio > 3.5, "Hit limit too frequently: Ratio=%.2f" % ratio
-        assert ratio < 6.5, "Hit limit too infrequently: Ratio=%.2f" % ratio
+        assert ratio > 4.5, "Hit limit too frequently: Ratio=%.2f" % ratio
+        assert ratio < 7.5, "Hit limit too infrequently: Ratio=%.2f" % ratio
