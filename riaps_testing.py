ff7a12a55fe4df29f4eb76e4bd65115c0475efcd
Joey Holliday <joeyholliday77@gmail.com>
2018-10-15 17:00:03 -0500

    Loosen timings to give nodes more time to connect

--- riaps_testing.py
+++ riaps_testing.py
@@ -57,7 +57,7 @@ def powerCycleControl(hostname, events):
         sock.send_pyobj(msg)
         assert sock.recv_pyobj() == 'Powercycle sent', "Powercycle failed for timediff: %d" % timediff
 
-def runTest(name, folder, riaps, depl, startupTime=10, runTime=45, cleanupTime=10, powerTiming=None):
+def runTest(name, folder, riaps, depl, startupTime=60, runTime=60, cleanupTime=10, powerTiming=None):
     """Run a RIAPS application on BBB hosts
 
     Args:
