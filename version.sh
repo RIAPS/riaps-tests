e8d34a849b6a35b53e0877dc61931c2913a9bc7a
Joey Holliday <joeyholliday77@gmail.com>
2018-10-22 17:12:29 -0500

    Update to release v1.1.10

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-export externalsversion="develop" 
+export externalsversion="1.1.10" 
 export coreversion="1.1.10"
 export pycomversion="1.1.10"
-export timesyncversion="1.1.9"
+export timesyncversion="1.1.10"
