2fdbbc3912b712f1c2fae61c3fdbfcbbf74affa4
timkrentz <tskrentz@gmail.com>
2019-07-18 15:23:14 -0500

    Storage consumption fixed to write into app folder
    - Storage consumption moved away from bash 'dd' because AppArmor gave that problems with filepaths, and
    switching to python subprocess.run() didn't work because AppArmor prevented accessing /bin/sh. Space
    is now wasted by writing a bytearray with python file IO.

--- testLimitSpace/SpcLimit.py
+++ testLimitSpace/SpcLimit.py
@@ -2,10 +2,10 @@
 from riaps.run.comp import Component
 import spdlog as spd
 import random
-import os
 import time
-import random, string
-import subprocess
+import string
+import os
+from pathlib import Path
 
 # Space  limited component
 
@@ -22,49 +22,37 @@ class SpcLimit(Component):
         logpath = '/tmp/riaps_SpcLimit_%d.log' % self.id
         remove(logpath)
 
-        self.logger = spd.FileLogger("testLimitSpace",logpath)
+        self.logger = spd.FileLogger("SpcLimit",logpath)
         self.logger.set_level(spd.LogLevel.DEBUG)
         self.logger.set_pattern("%v")
 
         self.logger.info("Starting SpcLimit %d" % self.id)
-        self.chain = []
         self.delta = 1 # 1 MB
         self.size = self.delta
-        self.name = '/home/riaps/riaps_apps/test_LimitSpace/tmp.junk'
-        remove(self.name)
-
         self.ticks = 0
 
-            
-    def waste(self):
-        cmd = 'dd if=/dev/zero of=%s bs=1M count=%d' % (self.name,self.size)
-        try:
-            res = os.system(cmd)
-        except Exception as e:
-            self.logger.info("os.system error: %s",str(e))
-        #res = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8',shell=True)
-        if res == 0:
-            self.size += self.delta
-        else:
-            self.logger.info("Failed %d with error code %d" % (self.size,res))
-        
+        self.junkpath = '/home/riaps/riaps_apps/test_LimitSpace/tmp.junk'
+        Path(self.junkpath).touch()
+
+
     def on_ticker(self):
-        self.ticks += 1
         trg = self.ticker.recv_pyobj()              # Receive time (as float)
-        now = time.time() 
+        self.ticks += 1
         self.logger.info('Tick %d' % self.size)
-        self.waste()
-        
+        with open(self.junkpath,'ab') as f:
+            f.write(bytearray(1000000))
+
     def handleSpcLimit(self):
         # Ignore double callbacks
         if self.ticks > 1:
             self.logger.info('Limit %d' % self.size)
-            remove(self.name)
             self.size = self.delta
             self.ticks = 0
-   
+            remove(self.junkpath)
+            Path(self.junkpath).touch()
+
     def __destroy__(self):
-        remove(self.name)
+        remove(self.junkpath)
         self.logger.info("Stopping SpcLimit %d" % self.id)
         self.logger.flush()
         self.logger.close()
