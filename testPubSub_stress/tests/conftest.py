465bea45990bf8917dc8caad088c07d63aa01444
Scott Eisele <eiselesr@gmail.com>
2023-10-24 12:49:28 -0500

    comment out dependency on fabric2

--- testPubSub_stress/tests/conftest.py
+++ testPubSub_stress/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 # from riaps.test_suite.fixtures.remote_connection import fabric_group
-from riaps.test_suite.fixtures.remote_connection import setup_remote_tmux
+# from riaps.test_suite.fixtures.remote_connection import setup_remote_tmux
 from riaps.test_suite.fixtures.LogServer import log_server, platform_log_server
 from riaps.test_suite.fixtures.utils import test_logger
 from riaps.test_suite.fixtures.pytest_logger import testslogger
